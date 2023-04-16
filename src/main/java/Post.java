101ba52eeb2ddc33524cabe28ace91475425df32
kristinayax <kristinayax@icloud.com>
2023-04-16 17:41:20 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+    FormDate birthday = new FormDate();
+}
